1d169bdb1106517d8c1c6e4d5f7bc5a84c2e2465
bruno <brunomestres@gmail.com>
2022-06-21 18:13:37 -0300

    Add Producer or Activist

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Login from "../pages/login";
 import Dashboard from '../pages/dashboard';
+import Register from "../pages/register";
 
 export default function Router(){
     return(
@@ -9,6 +10,7 @@ export default function Router(){
             <Routes>
                 <Route exact path="/" element={<Login/>}/>
                 <Route exact path="/dashboard/:walletAddress" element={<Dashboard/>}/>
+                <Route exact path="/register" element={<Register/>}/>
             </Routes>
         </BrowserRouter>
     );
